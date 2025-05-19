a1f6f405624b68fa14cc33a3bf1a56aba68e1d3f
Hangyeol Rhim <hgyeol194@gmail.com>
2025-05-19 13:46:22 +0900

    🚀 CRA 기반 UI 프로젝트 업로드

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const App = () => <h1>🧊 위스키 UI 앱에 오신 걸 환영합니다!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
